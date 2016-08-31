35ac469eee16c2c737008e224111047f5868b8dc
verbumdie <verbumdie@gmail.com>
2016-08-31 05:56:34 +0000

    Add initial assets

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # jojoingresswotd
 
-The home of Jojo Stratton's Word of the Day on GitHub.
+The home of Jojo Stratton's Word of the Day
+
---
+++ jsc/wotd.js
@@ -0,0 +1,3 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);s.onreadystatechange=function(){s.readyState==='interactive'&&function(){[].concat(Array.prototype.slice.call(s.getElementsByTagName('code')),Array.prototype.slice.call(s.getElementsByClassName('code'))).forEach(function(e){e.onclick=function(){e.classList.toggle('reveal')}})}()};})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-54256624-2', 'auto');
+ga('send', 'pageview');
